bcd2867bdf9cd6475aa3411e842f8be830b02eef
Kevin Gori <kgori@ebi.ac.uk>
2015-11-03 18:19:57 +0000

    Added support for building using clang in Mac OS --- seems the only way to do it since El Crapitan

--- setup.py
+++ setup.py
@@ -5,8 +5,25 @@ except ImportError:
 
 from Cython.Distutils import build_ext
 import pkg_resources
+import platform, re, subprocess
 
-# data_dir = pkg_resources.resource_filename("autowrap", "data_files")
+def is_clang(bin):
+    proc = subprocess.Popen([bin, '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    output = '\n'.join([stdout, stderr])
+    return not re.search(r'clang', output) is None
+
+class my_build_ext(build_ext):
+    def build_extensions(self):
+        binary = self.compiler.compiler[0]
+        if is_clang(binary):
+            for e in self.extensions:
+                e.extra_compile_args.append('-stdlib=libc++')
+                if platform.system() == 'Darwin':
+                    e.extra_compile_args.append('-mmacosx-version-min=10.7')
+                    e.extra_link_args.append('-mmacosx-version-min=10.7')
+
+        build_ext.build_extensions(self)
 
 ext = Extension("tree_distance",
                 language='c++',
@@ -19,14 +36,14 @@ ext = Extension("tree_distance",
                            'Ratio.cpp',
                            'RatioSequence.cpp',
                            'Tools.cpp',
-                           'cython/TreeDistance.pyx'],
+                           'cython/tree_distance.pyx'],
                 # include_dirs = [data_dir],
                 extra_compile_args=['-std=c++11'],
                )
 
-setup(cmdclass={'build_ext':build_ext},
+setup(cmdclass={'build_ext':my_build_ext},
       name="tree_distance",
-      version="0.0.6",
+      version="0.0.7",
       author='Kevin Gori',
       author_email='kgori@ebi.ac.uk',
       description='Wrapper for GTP tree distances in c++',
