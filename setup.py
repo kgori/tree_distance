703ba38c622e6d25e48b644a4213843b460ef975
Kevin Gori <kcg25@cam.ac.uk>
2024-08-12 13:11:38 +0100

    Update actions

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ ext = Extension("tree_distance",
 
 setup(cmdclass={'build_ext':my_build_ext},
       name="tree_distance",
-      version="1.0.10",
+      version="1.0.11",
       author='Kevin Gori',
       author_email='kgori@ebi.ac.uk',
       description='Wrapper for GTP tree distances in c++',
